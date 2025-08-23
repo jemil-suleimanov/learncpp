bfc28def1d22a3221bc6801ab5d9016f9cf43616
jemil-suleimanov <jemil1931@gmail.com>
2025-08-23 12:25:19 +0200

    Day 2025-08-23

---
+++ 7-chapter/7.7-exnternal-link/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+extern int g_x;       // this extern is a forward declaration of a variable named g_x that is defined somewhere else
+extern const int g_y; // this extern is a forward declaration of a const variable named g_y that is defined somewhere else
+
+void sayHi();
+
+int main()
+{
+    sayHi();
+
+    std::cout << g_x << ' ' << g_y << '\n'; // prints 2 3
+
+    return 0;
+}
