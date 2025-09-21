2cec24a82d1ed59aa3f8920561b7dd53b0935a31
jemil-suleimanov <jemil1931@gmail.com>
2025-09-21 19:32:36 +0200

    Day 2025-09-21

---
+++ 11-chapter/11.10-multiple-files-template-func/add.h
@@ -0,0 +1,10 @@
+#ifndef ADD_H
+#define ADD_H
+
+template <typename T>
+T addOne(T x)
+{
+    return x + 1;
+}
+
+#endif
---
+++ 11-chapter/11.10-multiple-files-template-func/main.cpp
@@ -0,0 +1,10 @@
+#include "add.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << addOne(1);
+    std::cout << addOne(2.3);
+
+    return 0;
+}
