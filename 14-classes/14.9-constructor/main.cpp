d6acce0a0625e5eade25c6a0cb33408812eef337
jemil-suleimanov <jemil1931@gmail.com>
2025-09-30 23:59:22 +0200

    Day 2025-09-30

---
+++ 14-classes/14.9-constructor/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Foo
+{
+private:
+    int m_x {};
+    int m_y {};
+
+public:
+    Foo(int x, int y)
+    {
+        std::cout << "Foo(" << x << ", " << y << ") constructed\n";
+    }
+
+    void print() const
+    {
+        std::cout << "Foo(" << m_x << ", " << m_y << ")\n";
+    }
+};
+
+int main()
+{
+    Foo foo{ 6, 7};
+    foo.print();
+
+    return 0;
+}
