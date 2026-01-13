c11f7c8df331aa5c7258af24235982fe77bc0297
jemil-suleimanov <jemil1931@gmail.com>
2026-01-13 23:06:30 +0100

    refactor Card class and implement player/dealer turn logic in main game loop

--- 17-chapter/17-summary/question-3/main.cpp
+++ 17-chapter/17-summary/question-3/main.cpp
@@ -2,6 +2,15 @@
 #include <algorithm> // for std::shuffle
 #include "Random.h"  // for Random::mt
 
+namespace Settings
+{
+    // Maximum score before losing.
+    constexpr int bust{ 21 };
+
+    // Minium score that the dealer has to have.
+    constexpr int dealerStopsAt{ 17 };
+}
+
 struct Card
 {
     enum Rank
@@ -87,31 +96,21 @@ struct Card
         }
     }
 
-    static constexpr int getValue(Card::Rank rank)
+    int value() const
     {
-        switch(rank) {
-            case Card::rank_ace: return 11;
-            case Card::rank_2: return 2;
-            case Card::rank_3: return 3;
-            case Card::rank_4: return 4;
-            case Card::rank_5: return 5;
-            case Card::rank_6: return 6;
-            case Card::rank_7: return 7;
-            case Card::rank_8: return 8;
-            case Card::rank_9: return 9;
-            case Card::rank_10: return 10;
-            case Card::rank_jack: return 10;
-            case Card::rank_queen: return 10;
-            case Card::rank_king: return 10;
-            default: return -1;
-        }
-    } 
+        static constexpr std::array rankValues { 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 };
+        return rankValues[rank];
+    }
 
     friend std::ostream& operator<<(std::ostream& out, const Card &card)
     {
         out << Card::getCardRank(card.rank) << Card::getCardSuit(card.suit);
         return out;
     }
+};
+
+struct Player {
+    int score {};
 };
 
 class Deck
@@ -152,28 +151,91 @@ public:
     }
 };
 
-int main()
+void ignoreLine()
 {
-    Card card { Card::rank_5, Card::suit_heart };
-    std::cout << card << '\n';
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
 
-    for (auto suit: Card::allSuits)
+char getInput()
+{
+    char input {};
+    std::cout << "(h) tp hit, or (s) to stand: ";
+    std::cin >> input;
+    ignoreLine();
+
+    return input;
+}
+
+bool playerTurn(Deck& deck, Player& player)
+{
+    while(player.score <= Settings::bust)
     {
-        for (auto rank: Card::allRanks)
+        char decision { getInput() };
+        if (decision == 's')
         {
-            std::cout << Card { rank, suit } << ' ';
+            break;
+            return false;
         }
+
+        Card card { deck.dealCard() };
+        player.score += card.value();
+        std::cout << "You were dealt " << card << ". You now have: " << player.score << '\n';
+        if (player.score > Settings::bust)
+            return true;
     }
 
-    std::cout << '\n';
+    return false;
+}
 
+bool dealerTurn(Deck& deck, Player& dealer)
+{
+    while(dealer.score < Settings::dealerStopsAt)
+    {
+        Card card { deck.dealCard() };
+        std::cout << "The dealer flips " << card << '\n';
+        dealer.score += card.value();
+    }
+
+    if (dealer.score > Settings::bust) {
+        std::cout << "The dealer went bust!\n";
+        return true;
+    }
+
+    return false;
+}
+
+bool playRound()
+{
     Deck deck{};
-
-    std::cout << deck.dealCard() << ' ' << deck.dealCard() << ' ' << deck.dealCard() << '\n';
-
     deck.shuffle();
+    Player player {0};
+    Player dealer {0};
 
-    std::cout << deck.dealCard() << ' ' << deck.dealCard() << ' ' << deck.dealCard() << '\n';
+    dealer.score += deck.dealCard().value();
+    player.score += deck.dealCard().value() + deck.dealCard().value();
+
+    std::cout << "The dealer is showing: " << dealer.score << '\n';
+    std::cout << "You have score : " << player.score << '\n';
+
+    if (playerTurn(deck, player)) {
+        std::cout << "You went bust!\n";
+        return false;
+    }
+
+    if (dealerTurn(deck, dealer))
+    {
+        std::cout << "The dealer went bust!\n";
+        return true;
+    }
+
+    return player.score > dealer.score;
+}
+
+int main()
+{
+    bool isWin {playRound()};
+
+    std::cout << "You " << (isWin ? "win" : "lose") << "!\n";
 
     return 0;
 }
